cea054f58795e9ea146f3e9fb3b8a9e0a5a81aae
Alejandro Vigi <marco.vigi@ingenieria.unam.edu>
2025-05-26 19:05:40 -0600

    Eliminando hardcoding en el gradle.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,16 +49,12 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-
     // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-
+    implementation(libs.retrofit)
     // Gson Converter
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
+    implementation(libs.converter.gson)
     // Coil
-    implementation("io.coil-kt:coil-compose:2.6.0")
-
+    implementation(libs.coil.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
